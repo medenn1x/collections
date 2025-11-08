448b16200275267d2f7c40935bf09b30b22ad0e9
Marcus Dennis <marcusedennis@gmail.com>
2025-11-07 16:41:32 -0800

    Add unit tests for internal views

---
+++ src/test/java/net/arinoru/collections/ViewsTest.java
@@ -0,0 +1,355 @@
+package net.arinoru.collections;
+
+import net.arinoru.collections.Views.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import java.util.Iterator;
+import java.util.PrimitiveIterator;
+import java.util.function.Consumer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.Mockito.*;
+
+@SuppressWarnings({"unchecked", "ResultOfMethodCallIgnored"})
+class ViewsTest {
+    @Test
+    public void forEachRemaining__pureIteratorView__forwardsRequest() {
+        var action = (Consumer<Object>) mock(Consumer.class);
+        var iterator = mock(Iterator.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Object>>(iterator,
+                ForwardingType.PURE, MaskingType.DELEGATE);
+        var cut = new IteratorView<>(forwarder);
+
+        cut.forEachRemaining(action);
+
+        verify(iterator).forEachRemaining(action);
+        verifyNoMoreInteractions(action, iterator);
+    }
+
+    @ParameterizedTest
+    @EnumSource(names = {"SHALLOW","MINIMAL"})
+    public void forEachRemaining__shallowOrMinimalIteratorView__processesRequest(ForwardingType forwardingType) {
+        var action = (Consumer<Object>) mock(Consumer.class);
+        var iterator = mock(Iterator.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Object>>(iterator,
+                forwardingType, MaskingType.DELEGATE);
+        var cut = new IteratorView<>(forwarder);
+        var ob1 = new Object();
+        var ob2 = new Object();
+        when(iterator.hasNext()).thenReturn(true, true, false);
+        when(iterator.next()).thenReturn(ob1, ob2);
+
+        cut.forEachRemaining(action);
+
+        verify(iterator, never()).forEachRemaining(any());
+        verify(iterator, times(3)).hasNext();
+        verify(iterator, times(2)).next();
+        verify(action).accept(ob1);
+        verify(action).accept(ob2);
+        verifyNoMoreInteractions(action, iterator);
+    }
+
+    @ParameterizedTest
+    @EnumSource(ForwardingType.class)
+    public void hasNext__iteratorView__forwardsRequest(ForwardingType forwardingType) {
+        var iterator = mock(Iterator.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Object>>(iterator,
+                forwardingType, MaskingType.DELEGATE);
+        var cut = new IteratorView<>(forwarder);
+
+        cut.hasNext();
+
+        verify(iterator).hasNext();
+        verifyNoMoreInteractions(iterator);
+    }
+
+    @ParameterizedTest
+    @EnumSource(ForwardingType.class)
+    public void next__iteratorViewNotBackedByPrimitiveIterator__forwardsRequest(ForwardingType forwardingType) {
+        var iterator = mock(Iterator.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Object>>(iterator,
+                forwardingType, MaskingType.DELEGATE);
+        var cut = new IteratorView<>(forwarder);
+        var ob = new Object();
+        when(iterator.next()).thenReturn(ob);
+
+        var result = cut.next();
+
+        assertThat(result).isSameAs(ob);
+        verify(iterator).next();
+        verifyNoMoreInteractions(iterator);
+    }
+
+    @Test
+    public void next__pureIteratorViewBackedByDoubleIterator__forwardsRequest() {
+        var iterator = mock(PrimitiveIterator.OfDouble.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Double>>(iterator,
+                ForwardingType.PURE, MaskingType.DELEGATE);
+        var cut = new IteratorView<>(forwarder);
+
+        cut.next();
+
+        verify(iterator).next();
+        verifyNoMoreInteractions(iterator);
+    }
+
+    @ParameterizedTest
+    @EnumSource(names = {"SHALLOW","MINIMAL"})
+    public void next__shallowOrMinimalIteratorViewBackedByDoubleIterator__processesRequest(ForwardingType forwardingType) {
+        var iterator = mock(PrimitiveIterator.OfDouble.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Double>>(iterator,
+                forwardingType, MaskingType.DELEGATE);
+        var cut = new IteratorView<>(forwarder);
+        when(iterator.nextDouble()).thenReturn(1.0);
+
+        var result = cut.next();
+
+        assertThat(result.doubleValue()).isEqualTo(1.0);
+        verify(iterator).nextDouble();
+        verifyNoMoreInteractions(iterator);
+    }
+
+    @Test
+    public void next__pureIteratorViewBackedByIntIterator__forwardsRequest() {
+        var iterator = mock(PrimitiveIterator.OfInt.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Integer>>(iterator,
+                ForwardingType.PURE, MaskingType.DELEGATE);
+        var cut = new IteratorView<>(forwarder);
+
+        cut.next();
+
+        verify(iterator).next();
+        verifyNoMoreInteractions(iterator);
+    }
+
+    @ParameterizedTest
+    @EnumSource(names = {"SHALLOW","MINIMAL"})
+    public void next__shallowOrMinimalIteratorViewBackedByIntIterator__processesRequest(ForwardingType forwardingType) {
+        var iterator = mock(PrimitiveIterator.OfInt.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Integer>>(iterator,
+                forwardingType, MaskingType.DELEGATE);
+        var cut = new IteratorView<>(forwarder);
+        when(iterator.nextInt()).thenReturn(1);
+
+        var result = cut.next();
+
+        assertThat(result.intValue()).isEqualTo(1);
+        verify(iterator).nextInt();
+        verifyNoMoreInteractions(iterator);
+    }
+
+    @Test
+    public void next__pureIteratorViewBackedByLongIterator__forwardsRequest() {
+        var iterator = mock(PrimitiveIterator.OfLong.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Long>>(iterator,
+                ForwardingType.PURE, MaskingType.DELEGATE);
+        var cut = new IteratorView<>(forwarder);
+
+        cut.next();
+
+        verify(iterator).next();
+        verifyNoMoreInteractions(iterator);
+    }
+
+    @ParameterizedTest
+    @EnumSource(names = {"SHALLOW","MINIMAL"})
+    public void next__shallowOrMinimalIteratorViewBackedByLongIterator__processesRequest(ForwardingType forwardingType) {
+        var iterator = mock(PrimitiveIterator.OfLong.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Long>>(iterator,
+                forwardingType, MaskingType.DELEGATE);
+        var cut = new IteratorView<>(forwarder);
+        when(iterator.nextLong()).thenReturn(1L);
+
+        var result = cut.next();
+
+        assertThat(result.longValue()).isEqualTo(1L);
+        verify(iterator).nextLong();
+        verifyNoMoreInteractions(iterator);
+    }
+
+    @ParameterizedTest
+    @EnumSource(ForwardingType.class)
+    public void remove__shallowOrMinimalIteratorView__forwardsRequest(ForwardingType forwardingType) {
+        var iterator = mock(Iterator.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Object>>(iterator,
+                forwardingType, MaskingType.DELEGATE);
+        var cut = new IteratorView<>(forwarder);
+
+        cut.remove();
+
+        verify(iterator).remove();
+        verifyNoMoreInteractions(iterator);
+    }
+
+    @Test
+    public void forEachRemaining__pureUnmodifiableIteratorView__forwardsRequest() {
+        var action = (Consumer<Object>) mock(Consumer.class);
+        var iterator = mock(Iterator.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Object>>(iterator,
+                ForwardingType.PURE, MaskingType.UNMODIFIABLE);
+        var cut = new UnmodifiableIteratorView<>(forwarder);
+
+        cut.forEachRemaining(action);
+
+        verify(iterator).forEachRemaining(action);
+        verifyNoMoreInteractions(action, iterator);
+    }
+
+    @ParameterizedTest
+    @EnumSource(names = {"SHALLOW","MINIMAL"})
+    public void forEachRemaining__shallowOrMinimalUnmodifiableIteratorView__processesRequest(ForwardingType forwardingType) {
+        var action = (Consumer<Object>) mock(Consumer.class);
+        var iterator = mock(Iterator.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Object>>(iterator,
+                forwardingType, MaskingType.UNMODIFIABLE);
+        var cut = new UnmodifiableIteratorView<>(forwarder);
+        var ob1 = new Object();
+        var ob2 = new Object();
+        when(iterator.hasNext()).thenReturn(true, true, false);
+        when(iterator.next()).thenReturn(ob1, ob2);
+
+        cut.forEachRemaining(action);
+
+        verify(iterator, never()).forEachRemaining(any());
+        verify(iterator, times(3)).hasNext();
+        verify(iterator, times(2)).next();
+        verify(action).accept(ob1);
+        verify(action).accept(ob2);
+        verifyNoMoreInteractions(action, iterator);
+    }
+
+    @ParameterizedTest
+    @EnumSource(ForwardingType.class)
+    public void hasNext__unmodifiableIteratorView__forwardsRequest(ForwardingType forwardingType) {
+        var iterator = mock(Iterator.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Object>>(iterator,
+                forwardingType, MaskingType.UNMODIFIABLE);
+        var cut = new UnmodifiableIteratorView<>(forwarder);
+
+        cut.hasNext();
+
+        verify(iterator).hasNext();
+        verifyNoMoreInteractions(iterator);
+    }
+
+    @ParameterizedTest
+    @EnumSource(ForwardingType.class)
+    public void next__unmodifiableIteratorViewNotBackedByPrimitiveIterator__forwardsRequest(ForwardingType forwardingType) {
+        var iterator = mock(Iterator.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Object>>(iterator,
+                forwardingType, MaskingType.UNMODIFIABLE);
+        var cut = new UnmodifiableIteratorView<>(forwarder);
+        var ob = new Object();
+        when(iterator.next()).thenReturn(ob);
+
+        var result = cut.next();
+
+        assertThat(result).isSameAs(ob);
+        verify(iterator).next();
+        verifyNoMoreInteractions(iterator);
+    }
+
+    @Test
+    public void next__pureUnmodifiableIteratorViewBackedByDoubleIterator__forwardsRequest() {
+        var iterator = mock(PrimitiveIterator.OfDouble.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Double>>(iterator,
+                ForwardingType.PURE, MaskingType.UNMODIFIABLE);
+        var cut = new UnmodifiableIteratorView<>(forwarder);
+
+        cut.next();
+
+        verify(iterator).next();
+        verifyNoMoreInteractions(iterator);
+    }
+
+    @ParameterizedTest
+    @EnumSource(names = {"SHALLOW","MINIMAL"})
+    public void next__shallowOrMinimalUnmodifiableIteratorViewBackedByDoubleIterator__processesRequest(ForwardingType forwardingType) {
+        var iterator = mock(PrimitiveIterator.OfDouble.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Double>>(iterator,
+                forwardingType, MaskingType.UNMODIFIABLE);
+        var cut = new UnmodifiableIteratorView<>(forwarder);
+        when(iterator.nextDouble()).thenReturn(1.0);
+
+        var result = cut.next();
+
+        assertThat(result.doubleValue()).isEqualTo(1.0);
+        verify(iterator).nextDouble();
+        verifyNoMoreInteractions(iterator);
+    }
+
+    @Test
+    public void next__pureUnmodifiableIteratorViewBackedByIntIterator__forwardsRequest() {
+        var iterator = mock(PrimitiveIterator.OfInt.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Integer>>(iterator,
+                ForwardingType.PURE, MaskingType.UNMODIFIABLE);
+        var cut = new UnmodifiableIteratorView<>(forwarder);
+
+        cut.next();
+
+        verify(iterator).next();
+        verifyNoMoreInteractions(iterator);
+    }
+
+    @ParameterizedTest
+    @EnumSource(names = {"SHALLOW","MINIMAL"})
+    public void next__shallowOrMinimalUnmodifiableIteratorViewBackedByIntIterator__processesRequest(ForwardingType forwardingType) {
+        var iterator = mock(PrimitiveIterator.OfInt.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Integer>>(iterator,
+                forwardingType, MaskingType.UNMODIFIABLE);
+        var cut = new UnmodifiableIteratorView<>(forwarder);
+        when(iterator.nextInt()).thenReturn(1);
+
+        var result = cut.next();
+
+        assertThat(result.intValue()).isEqualTo(1);
+        verify(iterator).nextInt();
+        verifyNoMoreInteractions(iterator);
+    }
+
+    @Test
+    public void next__pureUnmodifiableIteratorViewBackedByLongIterator__forwardsRequest() {
+        var iterator = mock(PrimitiveIterator.OfLong.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Long>>(iterator,
+                ForwardingType.PURE, MaskingType.UNMODIFIABLE);
+        var cut = new UnmodifiableIteratorView<>(forwarder);
+
+        cut.next();
+
+        verify(iterator).next();
+        verifyNoMoreInteractions(iterator);
+    }
+
+    @ParameterizedTest
+    @EnumSource(names = {"SHALLOW","MINIMAL"})
+    public void next__shallowOrMinimalUnmodifiableIteratorViewBackedByLongIterator__processesRequest(ForwardingType forwardingType) {
+        var iterator = mock(PrimitiveIterator.OfLong.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Long>>(iterator,
+                forwardingType, MaskingType.UNMODIFIABLE);
+        var cut = new UnmodifiableIteratorView<>(forwarder);
+        when(iterator.nextLong()).thenReturn(1L);
+
+        var result = cut.next();
+
+        assertThat(result.longValue()).isEqualTo(1L);
+        verify(iterator).nextLong();
+        verifyNoMoreInteractions(iterator);
+    }
+
+    @ParameterizedTest
+    @EnumSource(ForwardingType.class)
+    public void remove__shallowOrMinimalUnmodifiableIteratorView__throwsException(ForwardingType forwardingType) {
+        var iterator = mock(Iterator.class);
+        var forwarder = new Forwarder<Iterator<?>,Iterator<Object>>(iterator,
+                forwardingType, MaskingType.UNMODIFIABLE);
+        var cut = new UnmodifiableIteratorView<>(forwarder);
+
+        var t = catchThrowable(cut::remove);
+
+        assertThat(t).isInstanceOf(UnsupportedOperationException.class);
+        verifyNoMoreInteractions(iterator);
+    }
+}
